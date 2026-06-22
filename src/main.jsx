edca2778dd26840b0f8a0e3188bdc2a9d649f5da
roheda <rodrigoherreradavid@gmail.com>
2026-06-22 09:20:02 -0600

    Montar modulo estimaciones

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import FeedbackWidget from './FeedbackWidget.jsx'
+import EstimacionesWidget from './EstimacionesWidget.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <FeedbackWidget />
+    <EstimacionesWidget />
   </StrictMode>,
 )
